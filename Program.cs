f276b2a610f8a6f5e49aa745419be4b3c503ea39
Amanda Meneghini <admcmarshmallow@gmail.com>
2024-11-04 20:25:19 -0300

    Challenge solved

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Testando IPhone: ");
+
+Iphone iphone = new Iphone("(11)99999-9999","iPhone 11 Plus","5986b%", 64);
+iphone.InstalarAplicativo("Candy Crush");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+Console.WriteLine("\n");
+Console.WriteLine("Testando Nokia: ");
+
+Nokia nokia = new Nokia ("(21)98555-5555","Nokia G21","875444#", 128);
+nokia.InstalarAplicativo("Fortnite Mobile");
+nokia.Ligar();
+nokia.ReceberLigacao();
